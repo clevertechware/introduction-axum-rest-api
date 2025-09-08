3c9720691754004008ec868ab86fe7c7909ab564
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-09-09 00:48:29 +0200

    Setup hello world with axum and tokyo.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+use axum::Router;
+use axum::routing::get;
 use dotenvy::dotenv;
 use sqlx::postgres::PgPoolOptions;
+use tracing::info;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    // initialize tracing for logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgPoolOptions::new()
         .connect(&url)
         .await
         .expect("Failed to connect to database");
-    println!("Connected to the database!");
+    info!("Connected to the database!");
+
+    // build our application with a route
+    let app = Router::new()
+        // Get '/' goes to root
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 5000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8888").await.unwrap();
+    info!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+// handler for root
+async fn root() -> &'static str {
+    "Hello, World!"
 }
